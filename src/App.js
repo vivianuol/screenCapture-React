762a79560a8449c5ca1853964937d7877c16094d
Si,Peng <sipeng_vivian@hotmail.com>
2019-01-22 22:13:55 -0600

    the screenCapture

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ScreenShare from "./components/ScreenShare";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ScreenShare />
+      </div>
+    )
+  }
+}
+
+export default App;
